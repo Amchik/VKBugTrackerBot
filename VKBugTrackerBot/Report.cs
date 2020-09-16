6df7cfee583ac2b7570dd06dcc8623b509beb49c
Amchik <am4ik1337@gmail.com>
2020-09-16 21:42:40 +0500

    Add bookmarks

--- VKBugTrackerBot/Report.cs
+++ VKBugTrackerBot/Report.cs
@@ -1,28 +1,34 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace VKBugTrackerBot
 {
+    [Serializable]
     public sealed class Report
     {
         /// <summary>
         /// Gets the name.
         /// </summary>
         /// <value>The name.</value>
+        [JsonPropertyName("name")]
         public String Name { get; set; }
         /// <summary>
         /// Gets the product string.
         /// </summary>
         /// <value>The product.</value>
+        [JsonPropertyName("product")]
         public String Product { get; set; }
         /// <summary>
         /// Gets the tags.
         /// </summary>
         /// <value>The tags.</value>
+        [JsonPropertyName("tags")]
         public String[] Tags { get; set; }
         /// <summary>
         /// Gets the status string.
         /// </summary>
         /// <value>The status.</value>
+        [JsonPropertyName("status")]
         public String Status { get; set; }
 
         /// <summary>
@@ -33,11 +39,31 @@ namespace VKBugTrackerBot
         /// bugreport208755
         /// </example>
         [Obsolete("This property returns string value. Use Id for integer value", false)]
-        public String ReportID { get; set; }
+        [JsonIgnore]
+        public String ReportID { get => $"bugreport{Id}"; }
+
         /// <summary>
         /// Gets the report id.
         /// </summary>
         /// <value>The report id.</value>
+        [JsonPropertyName("id")]
         public Int32 Id { get; set; }
+
+        public override Int32 GetHashCode()
+        {
+            return this.Id;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null || obj as Report == null)
+            {
+                return false;
+            }
+
+            Report r = (Report)obj;
+
+            return this.Id == r.Id && this.Name == r.Name && this.Product == r.Product;
+        }
     }
 }
